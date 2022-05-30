9fb0f13b99bd6244b92ff89a5f980b5ce4fc9094
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2022-05-31 00:15:13 +0900

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+10.times do |i|
+  user = ::User.create(name: "user #{i + 1}")
+
+  rand(10).times do |i|
+    blog = user.blogs.create(title: "#{user.name}, title #{i + 1}")
+
+    rand(5).times do |i|
+      blog.comments.create(body: "#{blog.title}, comment #{i + 1}")
+    end
+  end
+end
