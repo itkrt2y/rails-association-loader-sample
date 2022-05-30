f3a77021108a4772c2cfbfbeb7ac9128291cd101
Tatsuya Itakura <itkrt2y.591721200@gmail.com>
2022-05-31 01:13:31 +0900

    Add order argument

---
+++ app/graphql/types/order_enum.rb
@@ -0,0 +1,6 @@
+module Types
+  class OrderEnum < BaseEnum
+    value "ASC", value: :asc
+    value "DESC", value: :desc
+  end
+end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -7,18 +7,32 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
-    field :blogs, [Types::BlogType], null: false
-
-    field :blog_dataloader, [Types::BlogType], null: false
-    def blog_dataloader
-      dataloader.with(Sources::Association, :blogs).load(object)
+    field :blogs, [Types::BlogType], null: false do
+      argument :order, Types::OrderEnum, required: false, default_value: :asc
+    end
+    def blogs(order:)
+      object.blogs.order(id: order)
     end
 
-    field :blogs_connection, Types::BlogType.connection_type, null: false, method: :blogs
+    field :blog_dataloader, [Types::BlogType], null: false do
+      argument :order, Types::OrderEnum, required: false, default_value: :asc
+    end
+    def blog_dataloader(order:)
+      dataloader.with(Sources::Association, :blogs, ::Blog.order(id: order)).load(object)
+    end
 
-    field :blogs_connection_dataloader, Types::BlogType.connection_type, null: false
-    def blogs_connection_dataloader
-      dataloader.with(Sources::Association, :blogs).load(object)
+    field :blogs_connection, Types::BlogType.connection_type, null: false do
+      argument :order, Types::OrderEnum, required: false, default_value: :asc
+    end
+    def blogs_connection(order:)
+      object.blogs.order(id: order)
+    end
+
+    field :blogs_connection_dataloader, Types::BlogType.connection_type, null: false do
+      argument :order, Types::OrderEnum, required: false, default_value: :asc
+    end
+    def blogs_connection_dataloader(order:)
+      dataloader.with(Sources::Association, :blogs, ::Blog.order(id: order)).load(object)
     end
   end
 end
